03410d6b1893dddd890e2cdad5b88cf23cc2941c
KateM3d <ekaterina8medvedeva@gmail.com>
2023-06-09 18:43:07 -0400

    first commit

---
+++ src/main/java/com/projectdocker/simpledockerapp/SimpleDockerAppApplication.java
@@ -0,0 +1,13 @@
+package com.projectdocker.simpledockerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDockerAppApplication.class, args);
+	}
+
+}
